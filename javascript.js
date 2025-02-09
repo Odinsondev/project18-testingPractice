e1df771e89e124ac301d834c8ad07d949f14631c
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2025-02-08 22:12:17 -0800

    Feat: add calculator function
    Feat: add caesarCipher function

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ function test1() {
   return true;
 }
 
-//A capitalize function that takes a string and returns it
+//1. A capitalize function that takes a string and returns it
 //with the first character capitalized.
 function capitalize(string) {
   const array = string.split('');
@@ -14,7 +14,7 @@ function capitalize(string) {
   return capitalisedString;
 }
 
-//A reverseString function that takes a string and returns it reversed.
+//2. A reverseString function that takes a string and returns it reversed.
 function reverse(string) {
   const array = string.split('');
   const reversedString = array.reverse().join('');
@@ -22,8 +22,82 @@ function reverse(string) {
   return reversedString;
 }
 
-//A calculator object that contains functions for the basic operations:
+//3. A calculator object that contains functions for the basic operations:
 //add, subtract, divide, and multiply.
 //Each of these functions should take two numbers and return the correct calculation.
+function createCalculator() {
+  const calc = {};
+  calc.add = function (num1, num2) {
+    return num1 + num2;
+  };
+  calc.subtract = function (num1, num2) {
+    return num1 - num2;
+  };
+  calc.multiply = function (num1, num2) {
+    return num1 * num2;
+  };
+  calc.divide = function (num1, num2) {
+    return num1 / num2;
+  };
+  return calc;
+}
 
-module.exports = { test1, capitalize, reverse };
+//4. A caesarCipher function that takes a string and a shift factor and
+//returns it with each character “shifted”.
+function caesarCipher(string, shift) {
+  const alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  const stringArray = string.split('');
+  const newStringArray = [];
+
+  //Iterates through every character in the string
+  for (let i = 0; i < stringArray.length; i++) {
+    let newLetterPosition = '';
+    let ifUpperCase = false;
+
+    let character = stringArray[i];
+
+    //Tests if character is a letter
+    if (/[a-zA-Z]/.test(character) === true) {
+      //Tests if uppercase
+      if (character === character.toUpperCase()) {
+        ifUpperCase = true;
+        let characterLower = character.toLowerCase();
+        character = characterLower;
+      }
+      //Finds letter location in alphabet and adds the shift value
+      for (let j = 0; j < alphabetArray.length; j++) {
+        if (character === alphabetArray[j]) {
+          const letterPosition = j;
+          newLetterPosition = letterPosition + shift;
+          if (newLetterPosition > 25) {
+            newLetterPosition -= 26;
+          }
+        }
+      }
+      //Finds the new letter and adds to the new array
+      for (let k = 0; k < alphabetArray.length; k++) {
+        if (k === newLetterPosition) {
+          //Checks if needing to convert to uppercase
+          if (ifUpperCase === true) {
+            newStringArray.push(alphabetArray[k].toUpperCase());
+          } else {
+            newStringArray.push(alphabetArray[k]);
+          }
+        }
+      }
+      //if character is not a letter, pushes it into the array (spaces, punctuation)
+    } else {
+      newStringArray.push(stringArray[i]);
+    }
+  }
+
+  console.log(newStringArray);
+  const result = newStringArray.join('');
+  return result;
+}
+
+//5. An analyzeArray function that takes an array of numbers and
+//returns an object with the following properties: average, min, max, and length.
+
+module.exports = { test1, capitalize, reverse, createCalculator, caesarCipher };
--- javascript.test.js
+++ javascript.test.js
@@ -1,4 +1,10 @@
-({ test1, capitalize, reverse } = require('./javascript.js'));
+({
+  test1,
+  capitalize,
+  reverse,
+  createCalculator,
+  caesarCipher,
+} = require('./javascript.js'));
 
 //initial test
 test('returns true', () => {
@@ -21,4 +27,38 @@ test('reverses the string', function () {
 
 test('reverses the string if several words', function () {
   expect(reverse('Random String')).toBe('gnirtS modnaR');
+});
+
+//calculator function tests
+test('add', function () {
+  expect(createCalculator().add(60, 9)).toBe(69);
+});
+
+test('subtract', function () {
+  expect(createCalculator().subtract(60, 9)).toBe(51);
+});
+
+test('multiply', function () {
+  expect(createCalculator().multiply(60, 9)).toBe(540);
+});
+
+test('divide', function () {
+  expect(createCalculator().divide(81, 9)).toBe(9);
+});
+
+//caesarCipher function tests
+test('uses cipher on test', function () {
+  expect(caesarCipher('test', 1)).toBe('uftu');
+});
+
+test('tests wrapping from z to a', function () {
+  expect(caesarCipher('xyz', 3)).toBe('abc');
+});
+
+test('tests case preservation', function () {
+  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+});
+
+test('tests punctuation', function () {
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
 });
